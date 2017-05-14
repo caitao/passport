9252e47ef208ce1f6682a3c512d228fa85a28f58
caitao <caotao@jx.chinamobile.com>
2017-05-14 23:48:44 +0800

    passport20170514 register error show

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,54 @@ var express = require('express');
 var router = express.Router();
 
 // register
-router.get('/register', function(req, res){
-	res.render('register');
+router.get('/register', function(req, res) {
+  res.render('register');
 });
 
 //login
-router.get('/login',  function(req, res){
-	res.render('login');
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+// register User
+router.post('/register', function(req, res) {
+  var name = req.body.name;
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  //validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render('register', {
+      errors: errors
+    });
+		console.log(errors)
+  } else {
+    var newUser = new User({
+      name: name,
+      username: username,
+      email: email,
+      password: password
+    });
+    User.createUser(newUser, function(err, user) {
+      if (err) throw err;
+      console.log(user);
+    });
+
+    req.flash('success_msg', 'You are registered and can now login');
+
+    res.redirect('/users/login');
+  }
 });
 
 module.exports = router;
